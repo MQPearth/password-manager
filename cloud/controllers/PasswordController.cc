721287719e235a41d72ccce3aee6bc9d22b1a2d2
thy <zzxjava@163.com>
2023-11-16 14:35:42 +0000

    !3 feat: update encode data * feat: update encode data

--- cloud/controllers/PasswordController.cc
+++ cloud/controllers/PasswordController.cc
@@ -55,5 +55,56 @@ void PasswordController::get(const HttpRequestPtr &req, std::function<void(const
 
 
 void PasswordController::update(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
+    auto json = req->getJsonObject();
+    if (!json) {
+        callback(Result::error());
+        return;
+    }
 
+    auto login_name = (*json)["login_name"];
+    if (login_name.empty() || login_name.asString().empty()) {
+        callback(Result::error("login_name不能为空"));
+        return;
+    }
+    auto password = (*json)["password"];
+    if (password.empty() || password.asString().empty()) {
+        callback(Result::error("password不能为空"));
+        return;
+    }
+
+    auto enc_data = (*json)["enc_data"];
+    if (enc_data.empty()) {
+        callback(Result::error("enc_data不能为null"));
+        return;
+    }
+
+    auto client = app().getDbClient()->newTransaction();
+
+    try {
+
+        orm::Mapper<Account> account_mapper(client);
+
+        auto row = account_mapper.findOne(orm::Criteria(Account::Cols::_login_name,
+                                                        orm::CompareOperator::EQ, login_name));
+
+        row.setLatestLoginDate(trantor::Date::now());
+        account_mapper.update(row);
+
+        orm::Mapper<Password> password_mapper(client);
+
+        auto enc_data_row = password_mapper.findOne(orm::Criteria(Password::Cols::_id,
+                                                                  orm::CompareOperator::EQ, *row.getId()));
+        enc_data_row.setEncData(enc_data.asString());
+
+        password_mapper.update(enc_data_row);
+
+        callback(Result::ok());
+    } catch (orm::UnexpectedRows &e) {
+        client->rollback();
+        callback(Result::error("账户名或密码错误"));
+    }
+    catch (...) {
+        client->rollback();
+        callback(Result::error());
+    }
 }
